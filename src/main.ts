d420a44b947d534da4fed5a7e52d18c14cd5e7fe
m-zs <mzasada93@gmail.com>
2021-09-14 19:09:42 +0200

    add logger to app

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
+import { configService } from './config/config.service';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform-interceptor';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
 
-  await app.listen(3000);
+  await app.listen(configService.getPort());
+  logger.log(`Listening at port ${configService.getPort()}`);
 }
 bootstrap();
